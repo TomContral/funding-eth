651d2cf6d7d47240503a42a7aeec2e02ee2facf5
well Guo <wellguo@gmail.com>
2018-11-05 21:31:45 +0800

    08.改造fundingFactroy，使之与InvestorToFunding匹配

--- funding/constracts/fundingFactroy.sol
+++ funding/constracts/fundingFactroy.sol
@@ -1,20 +1,25 @@
+
 pragma solidity ^0.4.24;
-    import './Untitled.sol';
+    import './funding.sol';
 
     //01.实现构造函数并测试成功
    contract fundingFactory{
         address[] public crowFundingArray;//所有众筹项目地址的集合
         mapping(address=>address[]) public creatorFundingMap;//当前账户发起的所有众筹项目的地址的集合
         address public platformProvider ;//众筹平台的提供者，合约仅部署一次。
+        InvestorToFunding investorToFunding;    //添加一个变量，默认i2为0x00000000000000000，必须实例化才可使用
+
 
    constructor()public{
         platformProvider=msg.sender;
+        address investorToFunding=new InvestorToFunding();//实例化InvestorToFunding合约，返回一个地址。
+
 
    }
    //02.创建新的合约
      function createFunding(string _fundingName,uint _supportBalance,uint _targetBalance,uint _endTime) public {
         //a. 创建一个新合约
-        address fundingAddress = new CrowFunding(_fundingName,_supportBalance,_targetBalance,_endTime,msg.sender);
+        address fundingAddress = new CrowFunding(_fundingName,_supportBalance,_targetBalance,_endTime,msg.sender,investorToFunding);
         //b. 添加到合约集合中
         crowFundingArray.push(fundingAddress);
 
@@ -31,7 +36,10 @@ function getAllFunding () public returns(address[]){
    function getCreatorFunding ()public returns(address[]){
        return creatorFundingMap[msg.sender];
    }
-
+   //获取当前账户所参加的所有合约
+      function getInvestorFunding() public view returns(address[]) {
+        return investorToFunding.getFundingBy(msg.sender);
+    }
 }
    //我参与的合约
     contract InvestorToFunding{
